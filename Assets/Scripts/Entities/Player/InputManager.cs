31c360323e780af2ff01a9de0e3463ee28824296
Matthew Ferreira <ChronoblastGamesOfficial@gmail.com>
2016-09-19 21:14:21 -0400

    Initial commit

---
+++ Assets/Scripts/Entities/Player/InputManager.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerController playerController;
+
+    [Header("Axis")]
+    public float xAxis;
+    public float yAxis;
+    [Header("Controls")]
+    public KeyCode Attach;
+    public KeyCode Switch;
+    [Header("Current Player")]
+    public GameObject currentPlayer;
+
+	void Awake ()
+    {
+        playerController = GetComponent<PlayerController>();
+	}
+	
+	void FixedUpdate ()
+    {
+        CheckForInputs();
+	}
+
+    public void CheckForInputs()
+    {
+        xAxis = Input.GetAxis("Horizontal");
+        yAxis = Input.GetAxis("Vertical");
+
+        if (xAxis != 0 || yAxis != 0)
+        {
+            if (xAxis > 0)
+            {
+
+            }
+
+            if (xAxis < 0)
+            {
+
+            }
+
+            if (yAxis > 0)
+            {
+
+            }
+
+            if (yAxis < 0)
+            {
+
+            }
+        }
+
+        if (Input.GetKeyDown(Attach))
+        {
+            playerController.Attach();
+        }
+
+        if (Input.GetKeyDown(Switch))
+        {
+            playerController.Switch();
+        }
+    }
+}
---
+++ Assets/Scripts/Entities/Player/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody myRB;
+    [Header("Player")]
+    public GameObject currentPlayer;
+    private GameObject player1;
+    private GameObject player2;
+    private GameObject player3;
+    private GameObject player4;
+
+    [Header("Player Variables")]
+    public float moveDistance;
+
+    [Header("Player Booleans")]
+    public bool canMove;
+    public bool canAttach;
+    public bool isAttachedLeft;
+    public bool isAttachedRight;
+
+    [Header("Layer Masks")]
+    public LayerMask attachMask;
+
+	void Start ()
+    {
+        myRB = GetComponent<Rigidbody>();
+	}
+	
+	void Update ()
+    {
+	
+	}
+
+    public void Move()
+    {
+
+    }
+
+    public void Attach()
+    {
+
+    }
+
+    public void Switch()
+    {
+
+    }
+
+    void RaycastCheck()
+    {
+        if (Physics.Raycast(transform.position, Vector3.left, moveDistance, attachMask))
+        {
+
+        }
+
+        if (Physics.Raycast(transform.position, Vector3.up, moveDistance, attachMask))
+        {
+
+        }
+
+        if (Physics.Raycast(transform.position, Vector3.right, moveDistance, attachMask))
+        {
+
+        }
+
+        if (Physics.Raycast(transform.position, Vector3.down, moveDistance, attachMask))
+        {
+
+        }
+    }
+}
