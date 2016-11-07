4df21eba741bc9846099ff0a8c07d5beac955b2c
Chronoblast <ChronoblastGamesOfficial@gmail.com>
2016-11-07 10:21:47 -0500

    Added Main Menu, Tweaked some Levels, Started Work on Scenery.

---
+++ Assets/Scripts/Systems/BackgroundMover.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundMover : MonoBehaviour 
+{
+    private TimerClass timerClass;
+
+    [Header ("Movement Variables")]
+    public float moveSpeed;
+    public float moveDelay;
+    public float moveAmount;
+
+    private Vector3 topPos;
+    private Vector3 bottomPos;
+
+    [Header ("Movement Booleans")]
+    public bool startedMoving;
+    public bool isGoingUp = true;
+    public bool isGoingDown;
+
+	void Start () 
+    {
+        moveDelay = Random.Range(1, 4);
+
+        timerClass = new TimerClass();
+        timerClass.ResetTimer(moveDelay);
+
+        CalculatePositions();
+	}
+	
+	void Update () 
+    {
+        Timer();
+
+        if (startedMoving)
+        {
+            Move();
+        }
+	}
+
+    void Move()
+    {
+        if (isGoingUp)
+        {
+            transform.position += new Vector3(0, moveSpeed, 0) * Time.deltaTime;
+        }
+
+        if (isGoingDown)
+        {
+            transform.position += new Vector3(0, -moveSpeed, 0) * Time.deltaTime;
+        }
+
+        if (transform.position.y > topPos.y)
+        {
+            isGoingUp = false;
+            isGoingDown = true;
+        }
+
+        if (transform.position.y < bottomPos.y)
+        {
+            isGoingDown = false;
+            isGoingUp = true;
+        }
+    }
+
+    void CalculatePositions()
+    {
+        topPos = transform.position + new Vector3(0, moveAmount, 0);
+        bottomPos = transform.position - new Vector3(0, moveAmount, 0);
+    }
+
+    void Timer()
+    {
+        if (!startedMoving)
+        {
+            if (timerClass.TimerIsDone())
+            {
+                startedMoving = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuManager : MonoBehaviour 
+{
+    [Header ("UI References")]
+    public GameObject optionsMenu;
+
+    [Header("UI Booleans")]
+    public bool isOptionsMenuOpen;
+
+    public void ManageOptionsMenu()
+    {
+        if (isOptionsMenuOpen)
+        {
+            optionsMenu.SetActive(false);
+            isOptionsMenuOpen = false;
+        }
+        else
+        {
+            optionsMenu.SetActive(true);
+            isOptionsMenuOpen = true;
+        }
+    }
+
+	public void LoadFirstLevel()
+    {
+        LevelManager.LoadNextLevel();
+    }
+
+    public void QuitGame()
+    {
+        LevelManager.QuitGame();
+    }
+}
